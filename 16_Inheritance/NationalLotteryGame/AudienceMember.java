609ab169676bd6ff7c5eb4060ca8906ab7955654
Commit-Here <burhanqibal86@gmail.com>
2024-06-05 07:43:57 +0530

    Branch for course task

---
+++ 16_Inheritance/NationalLotteryGame/AudienceMember.java
@@ -0,0 +1,7 @@
+// Representation of an audience member watching the lottery.
+public class AudienceMember extends Person {
+    // Constructor is given the person's name.
+    public AudienceMember(String name) (
+        
+    )
+}
