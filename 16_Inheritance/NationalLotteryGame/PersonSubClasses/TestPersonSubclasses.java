1f71828cdc0af4239c4b75392acabf4cc172aaa7
Commit-Here <burhanqibal86@gmail.com>
2024-06-07 09:55:04 +0530

    Added several subclasses of person class

---
+++ 16_Inheritance/NationalLotteryGame/PersonSubClasses/TestPersonSubclasses.java
@@ -0,0 +1,23 @@
+// Create one of each type of person, and make them speak.
+public class TestPersonSubclasses {
+    public static void main (String args[]){
+        Person[] persons = {
+            new AudienceMember("Ivana Di Yowt"),
+            new Director("Sir Lance Earl Otto"),
+            new Psychic("Miss T. Peg de Gowt"),
+            new Punter("Ian Arushfa Rishly Ving"),
+            new TVHost("Terry Bill Woah B'Gorne")
+        };
+
+        for (Person person: persons)
+            testPerson(person);
+    } // main
+
+    // Make the given person speak, reporting the before and after toString.
+    private static void testPerson(Person person) {
+        System.out.println("------------------------------------------------");
+        System.out.println(person);
+        person.speak();
+        System.out.println(person);
+    } // testPerson
+} // class TestPersonSubclasses
