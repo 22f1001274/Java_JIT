b16107e8ab636d5ad459e407f1839a6cb21dba5f
Commit-Here <burhanqibal86@gmail.com>
2024-06-14 09:37:04 +0530

    Added addititonal constructors for Book and MouseMat class

--- 16_Inheritance/NationalLotteryGame/StockItemSubClasses/Book/Book.java
+++ 16_Inheritance/NationalLotteryGame/StockItemSubClasses/Book/Book.java
@@ -1,10 +1,15 @@
 // Representation of book item.
 public class Book extends TextDescriptionStockItem {
 
+    // Constructor is given description, price and quantity
+    public Book(String desc, int price, int quantity){
+        super(desc, price, quantity);
+        vatRate = 0.0;
+    } // Book
+
     // Constructor is given price and quantity
     public Book(int price, int quantity){
-        super("default book item", price, quantity);
-        vatRate = 0.0;
+        this("default book item", price, quantity);
     } // Book
 
     // Returns the string "Stock item type"
--- 16_Inheritance/NationalLotteryGame/StockItemSubClasses/MouseMat/MouseMat.java
+++ 16_Inheritance/NationalLotteryGame/StockItemSubClasses/MouseMat/MouseMat.java
@@ -1,10 +1,16 @@
 // Representation of mouse mat item.
 public class MouseMat extends TextDescriptionStockItem {
 
+    // Constructor is given description, price and quantity
+    public MouseMat(String desc, int price, int quantity) {
+        super(desc, price, quantity);   
+    } // MouseMat
+
     // Constructor is given price and quantity.
     public MouseMat(int price, int quantity){
-        super("default mouse description", price, quantity);
+        this("default Mouse description", price, quantity);
     } // MouseMat
+
 
     // Returns the string "Stock item type"
     public String getStockType(){
