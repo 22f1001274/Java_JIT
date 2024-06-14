2e83907a535c20d40d75ea2ee257174a6710fb37
Commit-Here <burhanqibal86@gmail.com>
2024-06-14 09:25:57 +0530

    Class which manages purchase request, prints cart value and items in cart

---
+++ 16_Inheritance/NationalLotteryGame/ShoppingBasket/ShoppingBasket.java
@@ -0,0 +1,72 @@
+// Representation of a shopping basket which can contain
+// any number of stock item purchase requests.
+public class ShoppingBasket {
+    
+    // Number of stock item purchase requests
+    private StockItemPurchaseRequest[] stockItemPurchaseRequests;
+    
+    // The number of purchase requests.
+    private int noOfPurchaseRequests;
+
+    // The total shopping basket value ex vat.
+    private int baseketValueExVat;
+
+    // The total shopping basket value inc vat.
+    private int baseketValueIncVat;
+    
+    // The report of shopping basket.
+    private String shoppingBasketReport;
+
+    // Constructor 
+    public ShoppingBasket() {
+        noOfPurchaseRequests = 0;
+        baseketValueExVat = 0;
+        baseketValueIncVat = 0;
+        stockItemPurchaseRequests = new StockItemPurchaseRequest[noOfPurchaseRequests+1];
+    } // ShoppingBasket
+
+    // Add a given stock item with amount 
+    public void add(StockItem stockItem, int quantity) {
+        StockItemPurchaseRequest request = new StockItemPurchaseRequest(stockItem, quantity);
+        stockItemPurchaseRequests[noOfPurchaseRequests] = request;
+    } // add
+
+    //  
+    public String checkout() {
+
+        StockItemPurchaseRequest[] newStockItemPurchaseRequests = 
+                                new StockItemPurchaseRequest[noOfPurchaseRequests];
+        String checkoutReport = "Checkout report: \n";
+        int index = 0;
+
+        for (StockItemPurchaseRequest request : stockItemPurchaseRequests) {
+            StockItem stockItem = request.getStockItem();
+            int quantity = request.getQuantity();
+
+            baseketValueExVat += stockItem.getPriceExVat();
+            baseketValueIncVat += stockItem.getPriceIncVat();
+
+            if (stockItem.sellStock(quantity))
+                checkoutReport += "Purchased " + quantity + " of" + stockItem + "\n";
+            else {
+                checkoutReport += "Not Purchased " + quantity + " of" + stockItem + "\n";
+                newStockItemPurchaseRequests[index] = request;
+                index++;
+            } // else
+        } // for
+
+        stockItemPurchaseRequests = newStockItemPurchaseRequests;
+
+        checkoutReport += "Total price ex vat: " + baseketValueExVat + "p\n";
+        checkoutReport += "Total price inc vat: " + baseketValueIncVat + "p\n"; 
+
+        return checkoutReport;
+    } // checkout
+
+    public String toString() {
+        String result = "Shopping Basket: \n";
+        for (StockItemPurchaseRequest request : stockItemPurchaseRequests) 
+            result += request;
+        return result;
+    } // toString
+} // class ShoppingBasket
