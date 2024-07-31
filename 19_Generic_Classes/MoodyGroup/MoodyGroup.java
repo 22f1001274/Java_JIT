845853baa26fe32b279ec6d3d96143562faf49f2
Commit-Here <burhanqibal86@gmail.com>
2024-08-01 01:39:39 +0530

    MoodyGroup Project done

---
+++ 19_Generic_Classes/MoodyGroup/MoodyGroup.java
@@ -0,0 +1,48 @@
+public class MoodyGroup<Type extends MoodyPerson> {
+    // Initial size and resize factor
+    private final static int INTIAL_ARRAY_SIZE = 2, ARRAY_RESIZE_FACTOR = 2;
+
+    private MoodyPerson[] moodyPersons = new MoodyPerson[INTIAL_ARRAY_SIZE];
+    private int noOfPersons = 0;
+
+    // Empty constructor nothing needs to be done.
+    public MoodyGroup(){
+    }
+
+    public void addMoodyPerson(Type newPerson){
+        if (noOfPersons == moodyPersons.length){
+            MoodyPerson[] biggerArray = new MoodyPerson[moodyPersons.length * ARRAY_RESIZE_FACTOR];
+            for (int index=0; index<moodyPersons.length;index++)
+                biggerArray[index] = moodyPersons[index];
+            moodyPersons = biggerArray;
+        } // if
+        moodyPersons[noOfPersons] = newPerson;
+        noOfPersons++;
+    } // addPerson
+
+    public int getSize(){
+        return noOfPersons;
+    }
+
+    public MoodyPerson getMoodyPerson(int index){
+        return moodyPersons[index];
+    }
+
+    private int nextTo = 0;
+    public void setHappy(boolean status){
+        if (noOfPersons > 0)
+            for (int index =0; index<noOfPersons;index++){
+                moodyPersons[index].setHappy(status);
+                nextTo+=1;
+            }
+    
+    }
+
+    @Override 
+    public String toString(){
+        String result = noOfPersons == 0 ? "" : "" + moodyPersons[0];
+        for (int index = 1; index < noOfPersons; index++)
+            result += String.format("%n%s", moodyPersons[index]);
+        return result;
+    } // tostring
+}
---
+++ 19_Generic_Classes/MoodyGroup/TestMoodyGroup.java
@@ -0,0 +1,24 @@
+public class TestMoodyGroup{
+    public static void main(String[] args){
+        MoodyGroup<Teenager> teenagersGroup = new MoodyGroup<Teenager>();
+
+        teenagersGroup.addMoodyPerson(new Teenager("T1"));
+        teenagersGroup.addMoodyPerson(new Teenager("T2"));
+        teenagersGroup.addMoodyPerson(new Teenager("T3"));
+
+        System.out.printf("%s%n%n", teenagersGroup);
+        teenagersGroup.setHappy(true);
+        System.out.printf("%s%n%n", teenagersGroup);
+
+        MoodyGroup<MoodyPerson> commonGroup = new MoodyGroup<MoodyPerson>();
+        commonGroup.addMoodyPerson(new Worker("W1"));
+        commonGroup.addMoodyPerson((Teenager)teenagersGroup.getMoodyPerson(0));
+        commonGroup.addMoodyPerson((Teenager)teenagersGroup.getMoodyPerson(1));
+        commonGroup.addMoodyPerson((Teenager)teenagersGroup.getMoodyPerson(2));
+        
+        commonGroup.setHappy(true);
+        System.out.printf("%s%n%n", commonGroup);
+        commonGroup.setHappy(false);
+        System.out.printf("%s%n%n", commonGroup);
+    } // main
+} // class
